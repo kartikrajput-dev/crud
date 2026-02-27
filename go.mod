b2cba8c176eb0c1f23501825c7ceb1bb8e8379a9
kartikrajput-dev <kartikrajput.dev@gmail.com>
2026-02-27 11:12:48 +0000

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module apikit-crud
+
+go 1.25.4
+
+require (
+	github.com/KARTIKrocks/apikit v0.8.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import "github.com/KARTIKrocks/apikit"
+
+func main() {
+	InitDB()
+
+	app := apikit.New()
+
+	app.POST("/users", CreateUser)
+	app.GET("/users", GetUsers)
+	app.GET("/users/:id", GetUser)
+	app.PUT("/users/:id", UpdateUser)
+	app.DELETE("/users/:id", DeleteUser)
+
+	app.Run(":8080")
+}
